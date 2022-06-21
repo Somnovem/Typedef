b87cc19f2e53c81d94700e06bacec630d7f60407
Somnovem <artem.zhmura606@gmail.com>
2022-06-21 19:53:52 +0200

    adding new syntax

--- TypeDef/TypeDef/Source.cpp
+++ TypeDef/TypeDef/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
+using si_t = short int; //just better syntax than typedef(alias)
 int sum(int a, int b) {
 	return a + b;
 }
@@ -8,7 +9,7 @@ float sum(float a, float b) {
 
 	return a + b;
 }
-typedef unsigned long long ull_t;
+typedef unsigned long long ull_t; //type def
 unsigned long long smth = 10l;
 ull_t num2 = 100l;
 ull_t sum(ull_t a, ull_t b) {
