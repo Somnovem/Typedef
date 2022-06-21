ee1746bd7de5ef7f9def0c6c26f611f39d65eed0
Somnovem <artem.zhmura606@gmail.com>
2022-06-21 20:07:47 +0200

    Squashed commit of the following:
    commit b87cc19f2e53c81d94700e06bacec630d7f60407
    Author: Somnovem <artem.zhmura606@gmail.com>
    Date:   Tue Jun 21 19:53:52 2022 +0200
    
        adding new syntax
    
    commit 9c66723a820faec3325614baf029c7b9b600813a
    Author: Somnovem <artem.zhmura606@gmail.com>
    Date:   Tue Jun 21 19:51:11 2022 +0200
    
        repairing
    
    commit 46419b5fb2d191b0875d9d002b4f916bef1258d1
    Author: Somnovem <artem.zhmura606@gmail.com>
    Date:   Tue Jun 21 19:49:57 2022 +0200
    
        adding smth

--- TypeDef/TypeDef/Source.cpp
+++ TypeDef/TypeDef/Source.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <string>
 using namespace std;
+using si_t = short int; //just better syntax than typedef(alias)
+int sum(int a, int b) {
+	return a + b;
+}
+float sum(float a, float b) {
+
+	return a + b;
+}
+typedef unsigned long long ull_t; //type def
+unsigned long long smth = 10l;
+ull_t num2 = 100l;
+ull_t sum(ull_t a, ull_t b) {
+
+	return a + b;
+}
 int main() {
 	setlocale(LC_ALL, "Russian");
 
