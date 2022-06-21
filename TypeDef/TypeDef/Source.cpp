46419b5fb2d191b0875d9d002b4f916bef1258d1
Somnovem <artem.zhmura606@gmail.com>
2022-06-21 19:49:57 +0200

    adding smth

--- TypeDef/TypeDef/Source.cpp
+++ TypeDef/TypeDef/Source.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+int sum(int a, int b) {
+	return a + b;
+}
+float sum(float a, float b) {
+
+	return a + b;
+}
+typedef unsigned long long ull_t;
+unsigned long long = 10l;
+ull_t num2 = 100l;
+ull_t sum(ull_t a, ull_t b) {
+
+	return a + b;
+}
 int main() {
 	setlocale(LC_ALL, "Russian");
 
